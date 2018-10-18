bd5be34eb1d93655a5b8bf2f41dbf011a8df053e
Muka Schultze <samuelschultze@hotmail.com>
2018-10-18 16:38:50 -0300

    TempCultureInfo refactoring and moved to shared

---
+++ Assets/Scripts/Shared/TempCultureInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using GrandTheftAuto.Diagnostics;
+
+namespace GrandTheftAuto.Shared {
+    public class TempCultureInfo : IDisposable {
+
+        public CultureInfo CurrentCulture { get; private set; }
+        public CultureInfo PreviousCulture { get; private set; }
+
+        public TempCultureInfo(CultureInfo culture) {
+            PreviousCulture = CultureInfo.CurrentCulture;
+            CultureInfo.CurrentCulture = CurrentCulture = culture;
+            Log.Message("Changed culture info to \"{0}\"", culture);
+        }
+
+        public void Dispose() {
+            CultureInfo.CurrentCulture = PreviousCulture;
+            Log.Message("Reset current culture to \"{0}\"", PreviousCulture);
+        }
+
+    }
+}
